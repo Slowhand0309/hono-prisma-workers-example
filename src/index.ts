8ac03e3f2b5d986d67b3b324875ef93982d91255
Slowhand0309 <slowhand0309@gmail.com>
2024-04-18 00:01:59 +0900

    add /users route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { Hono } from 'hono'
+import { PrismaClient } from "@prisma/client/edge";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.get("/users", async (c) => {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  return c.json(users);
+});
+
+export default app;
