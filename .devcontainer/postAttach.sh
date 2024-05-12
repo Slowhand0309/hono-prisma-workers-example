884b6e7c1830c4d8030183e27db29bd570a06d48
Slowhand0309 <slowhand0309@gmail.com>
2024-05-12 12:12:02 +0900

    add pg

--- .devcontainer/postAttach.sh
+++ .devcontainer/postAttach.sh
@@ -6,4 +6,9 @@ sudo chown -R vscode node_modules
 sudo chown -R vscode /home/vscode/.config
 
 pnpm install
+
+# prisma
+pnpm prisma migrate dev
+pnpm prisma generate
+
 pnpm dev
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
+import { Pool } from "pg";
+
+// const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = "postgresql://postgres:postgres@db:5432/example";
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 const app = new Hono();
 
@@ -8,7 +17,6 @@ app.get("/", (c) => {
 });
 
 app.get("/users", async (c) => {
-  const prisma = new PrismaClient();
   const users = await prisma.user.findMany();
   return c.json(users);
 });
