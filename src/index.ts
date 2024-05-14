07668ac12ae01f3e8db5a79941b5fe655dccaa01
Slowhand0309 <slowhand0309@gmail.com>
2024-05-15 06:00:34 +0900

    refactor connection

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,6 @@ import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 import { Pool } from "pg";
 
-// const connectionString = `${process.env.DATABASE_URL}`;
-const connectionString = "postgresql://postgres:postgres@db:5432/example";
-
-const pool = new Pool({ connectionString });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
-
 const app = new Hono();
 
 app.get("/", (c) => {
@@ -17,6 +10,10 @@ app.get("/", (c) => {
 });
 
 app.get("/users", async (c) => {
+  const connectionString = `${c.env?.DATABASE_URL ?? ""}`;
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaPg(pool);
+  const prisma = new PrismaClient({ adapter });
   const users = await prisma.user.findMany();
   return c.json(users);
 });
